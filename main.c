22bbbd3d9fca94ea5e4498cde73d14ea1a340d95
Michelle Massaad <michelle.massaad@epita.fr>
2025-06-08 18:26:50 +0200

    done

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int main() {
     printf("Hello, EPITA!\n");
-    printf("vim commands practice");
+    printf("\"vim commands practice in .vimrc \" -Micha\n");
+    printf("1\t2\t3\n4\t5\t6\n"); 
     return 0;
 }
 
